27e777892ee0b41d857dd929c6a83d86450e652a
ericjung04 <ericjung1705@gmail.com>
2024-11-16 22:52:15 -0500

    Automated fetching and recording prices for analysis
    Now runs every 30 minutes.

--- src/r4cGlacier.js
+++ src/r4cGlacier.js
@@ -2,7 +2,8 @@ const { R6StatAPI} = require("r6statapi");
 require('dotenv').config({path: "../.env"});
 const api = new R6StatAPI();
 const email = process.env.EMAIL;
-const password = process.env.PASSWORD;
+const dbpassword = process.env.DB_PASSWORD;
+const apipassword = process.env.API_PASSWORD;
 const host = process.env.DB_HOST;
 const user = process.env.DB_USER;
 const cron = require("node-cron");
@@ -12,14 +13,14 @@ const ITEM_ID = "f619eb19-de6e-4dcd-96eb-08b45f80fe64";
 const pool = mysql.createPool({
     host: host,
     user: user,
-    password: password,
+    password: dbpassword,
     database: "R6Marketplace",
 });
 
 
 async function getPriceData() {
         try{
-        await api.login(email, password);
+        await api.login(email, apipassword);
         const details = await api.getItemDetails(ITEM_ID);
         const lastSalePrice = details.marktetData.lastSoldAt.price;
         const timestamp = details.marktetData.lastSoldAt.performedAt;
@@ -28,10 +29,54 @@ async function getPriceData() {
         console.log("Lowest Current Price:", lowestSalePrice);
         console.log("Last Sale Price:", lastSalePrice);
         console.log("Last Sold At:", formatTime);
-    }
-    catch (error){
-        console.log("Error:", error);
-    }
-    }
-getPriceData();
+        return [formatTime, lastSalePrice];
+        }
 
+        catch (error){
+            console.log("Error:", error);
+        }
+    }
+
+
+async function recordSale(itemID, soldAt, price)
+{
+    try{
+        const [rows] = await pool.query(
+            "SELECT * FROM prices WHERE item_id = ? AND sold_at = ?",
+          [itemID, soldAt]  
+        );
+
+        if (rows.length > 0)
+        {
+            console.log("No new sales.");
+            return;
+        }
+
+        await pool.query(
+            "INSERT INTO prices (item_id, sold_at, price) VALUES (?, ?, ?)",
+            [itemID, soldAt, price]          
+        );
+        console.log("New Sale at:", timestamp);
+    }
+
+    catch (error){
+        console.log("Error Adding Sale:", error);
+    }
+}
+
+async function getAndRecordSale()
+{
+    try {
+        let [x, y] = await getPriceData();
+        await recordSale(ITEM_ID, x, y);
+    } 
+    
+    catch (error) {
+        console.log(error);
+    }
+}
+
+cron.schedule("*/30 * * * *", async () => {
+    console.log("Checking Prices");
+    await getAndRecordSale();
+});
